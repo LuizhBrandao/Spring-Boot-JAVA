a05c00b27448cae7097617d9dcd64ca4104ac60e
LuizhBrandao <luizh.brandaoo@gmail.com>
2024-11-01 18:57:17 -0300

    exclusão de médicos e pacientes cadastrados.A exclusão não deve apagar os dados do médico e paciente, mas torná-los como "inativo" no sistema.

--- src/main/java/med/voll/vollmed/medico/Medico.java
+++ src/main/java/med/voll/vollmed/medico/Medico.java
@@ -29,9 +29,12 @@ public class Medico {
     private Especialidade especialidade;
 
     @Embedded
-    private med.voll.vollmed.endereco.Endereco endereco;
+    private Endereco endereco;
+
+    private Boolean ativo;
 
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -50,5 +53,9 @@ public class Medico {
         if (dados.endereco() != null){
             this.endereco.atualizarInformacoes(dados.endereco());
         }
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
